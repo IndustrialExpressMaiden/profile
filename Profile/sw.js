d2a5b76bb11a170c9bb2b860aebfd36ff984530e
ajshoeppner <50330118+ajshoeppner@users.noreply.github.com>
2019-05-23 13:07:58 -0500

    Created server worker

---
+++ Profile/sw.js
@@ -0,0 +1,38 @@
+let currentCache = "profile-v1";
+let itemsToCache = [
+  "index.html",
+  "css/ProfileStyle.css",
+  "image/face.jpg",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(currentCache).then(cache => {
+      return cache.addAll(itemsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheNames !== currentCache)
+        .map(cacheNames => caches.delete(cacheNames))
+      )}
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(currentCache).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(response => {
+          cache.put(event.request, response.clone())
+          return response;
+        })
+      })
+    })
+  );
+});
